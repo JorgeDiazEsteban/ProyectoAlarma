3ccc287cf5303774ae3592d5b46d9014aefbe13f
JorgeDiazEsteban <zarluca@hotmail.com>
2025-12-17 20:18:43 +0100

    AboutUs
    Creación inicial del about us

--- app/src/main/java/com/example/proyecto_alarma_pm/AboutUs.kt
+++ app/src/main/java/com/example/proyecto_alarma_pm/AboutUs.kt
@@ -7,7 +7,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class AboutUs : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_about_us)
