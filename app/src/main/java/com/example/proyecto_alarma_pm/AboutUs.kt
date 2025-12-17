63fc102a4c3616144f19733bea06d391d484999d
JorgeDiazEsteban <zarluca@hotmail.com>
2025-12-17 20:41:39 +0100

    binding y boton
    Añadido el binding de cierrre de app y la vuela a la pag principal en este caso va a ser el leer pill

--- app/src/main/java/com/example/proyecto_alarma_pm/AboutUs.kt
+++ app/src/main/java/com/example/proyecto_alarma_pm/AboutUs.kt
@@ -1,18 +1,29 @@
 package com.example.proyecto_alarma_pm
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.proyecto_alarma_pm.databinding.ActivityAboutUsBinding
+
 
 class AboutUs : AppCompatActivity() {
-    
+    private lateinit var binding: ActivityAboutUsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_about_us)
+        setContentView(binding.root)
+
+        binding.MainButton.setOnClickListener {
+            val intent = Intent(this, PillList::class.java)
+            startActivity(intent)
+            finish()
+        }
+        binding.CloseButton.setOnClickListener {
+            finishAffinity()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
